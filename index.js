6b54c2c5a33e7ffefeb8a8ed8129616b5443c0fe
Morgan Fudge <mfudge92@gmail.com>
2018-03-12 02:16:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ class Driver {
   }
 }
 
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
 class Route {
   constructor(a,b) {
     this.beginningLocation = a;
